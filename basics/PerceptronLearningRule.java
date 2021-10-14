927a0648d3d489338c94b071ca42178f43af8f67
daniel pirvu <danipedro2006@yahoo.com>
2021-10-14 09:19:33 +0300

    perceptron learning rule

---
+++ basics/PerceptronLearningRule.java
@@ -0,0 +1,67 @@
+package basics;
+
+import java.util.Arrays;
+
+public class PerceptronLearningRule {
+
+	public static void main(String[] args) {
+		double threshold = 1;
+		double learningRate = 0.1;
+		// Init weights
+		double[] weights = { 0.0, 0.0 };
+		// AND function training data
+		int[][][] trainingData = { { { 0, 0 }, { 0 } }, { { 0, 1 }, { 0 } }, { { 1, 0 }, { 0 } },
+				{ { 1, 1 }, { 1 } }, };
+
+		// Start learning loop
+
+		while (true) {
+
+			int errorCount = 0;
+			// Loop over training data
+			for (int i = 0; i < trainingData.length; i++) {
+
+				System.out.println("Starting weights: " + Arrays.toString(weights));
+				// Calculate weighted input
+				double weightedSum = 0;
+				for (int ii = 0; ii < trainingData[i][0].length; ii++) {
+					weightedSum += trainingData[i][0][ii] * weights[ii];
+				}
+
+				// Calculate output
+
+				int output = 0;
+				if (threshold <= weightedSum) {
+					output = 1;
+				}
+
+				System.out.println("Target output: " + trainingData[i][1][0] + ", " + "Actual Output: " + output);
+
+				// Calculate error
+
+				int error = trainingData[i][1][0] - output;
+				// Increase error count for incorrrect output
+
+				if (error != 0) {
+					errorCount++;
+				}
+
+				// Update weights
+				for (int ii = 0; ii < trainingData[i][0].length; ii++) {
+					weights[ii] += learningRate * error * trainingData[i][0][ii];
+				}
+
+				System.out.println("New weights: " + Arrays.toString(weights));
+				System.out.println();
+
+			}
+
+			// If there are no errors stop
+
+			if (errorCount == 0) {
+				System.out.println("Final weights: " + Arrays.toString(weights));
+				System.exit(0);
+			}
+		}
+	}
+}
